8791699b310b790eeaeab06baad9694ee1099ec8
jtiagosantos <tiagosan040@gmail.com>
2022-11-08 00:42:29 -0300

    chore: add mocks

---
+++ src/mock/favorites.mock.ts
@@ -0,0 +1,18 @@
+export const favorites = [
+  {
+    image: 'https://github.com/diego3g.png',
+    name: 'diego3g',
+  },
+  {
+    image: 'https://github.com/rodrigorgtic.png',
+    name: 'rodrigorgtic',
+  },
+  {
+    image: 'https://github.com/omariosouto.png',
+    name: 'omariosouto',
+  },
+  {
+    image: 'https://github.com/peas.png',
+    name: 'peas',
+  },
+];
