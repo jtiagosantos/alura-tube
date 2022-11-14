634a3fc4c4a8a0d9e4e1f5dc267d3c814b2f0c84
jtiagosantos <tiagosan040@gmail.com>
2022-11-13 21:17:24 -0300

    feat: integrate services and components in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { FormEvent, useEffect, useState } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
 import { X } from 'phosphor-react';
 import * as Tabs from '@radix-ui/react-tabs';
 
@@ -14,6 +16,7 @@ import {
   Dialog,
   Form,
   Iframe,
+  ActionButton,
 } from '@/common/styles/components';
 import {
   InputSearch,
@@ -22,13 +25,22 @@ import {
   AddNewVideoButton,
 } from '@/common/components';
 
+import { CreateOneVideoService } from '@/modules/video/services/create-one-video.service';
+import { CreateOnePlaylistService } from '@/modules/playlist/services/create-one-playlist.service';
+import { FindManyPlaylistsService } from '@/modules/playlist/services/find-many-playlists.service';
+import { FindManyVideosService } from '@/modules/video/services/find-many-videos.service';
+
 import { useForm } from '@/common/hooks/use-form.hook';
+import { useDisclosure } from '@/common/hooks/use-disclosure.hook';
 
 import { getVideoEmbedLink } from '@/common/utils/get-video-embed-link.util';
+import { getVideoThumbnailLink } from '@/common/utils/get-video-thumbnail-link.util';
+
+import { VideoEntity } from '@/modules/video/entities/video.entity';
+import { PlaylistEntity } from '@/modules/playlist/entities/playlist.entity';
 
 import { styled } from '@/common/styles/theme';
 
-import { playlists } from '@/mock/playlists.mock';
 import { favorites } from '@/mock/favorites.mock';
 
 const Main = styled('main', {
@@ -74,25 +86,55 @@ const TabsTrigger = styled(Tabs.Trigger, {
   },
 });
 
+const Select = styled('select', {
+  w: '$full',
+  h: '34px',
+  br: '8px',
+  borderWidth: '1px',
+  borderStyle: 'solid',
+  px: '$2',
+  mt: '-10px',
+  color: '$text_color_secondary',
+  fontSize: '$1',
+  bg: 'transparent',
+});
+
+const Option = styled('option', {
+  w: '$full',
+  bg: '#222222',
+});
+
 type TForm = {
-  title: string;
-  url: string;
+  video_title: string;
+  video_url: string;
+  video_playlist: string;
+  playlist_name: string;
 };
 
-export default function Home() {
-  const playlistsName = Object.keys(playlists) as [
-    'jogos' | 'front-end' | 'back-end',
-  ];
+type THomeServerProps = {
+  playlists: PlaylistEntity[];
+  data: Record<string, VideoEntity[]>;
+};
+
+export default function Home(props: THomeServerProps) {
+  const playlistsName = Object.keys(props.data);
 
   const { values, handleChange, clearFields } = useForm<TForm>({
-    title: '',
-    url: '',
+    video_title: '',
+    video_url: '',
+    video_playlist: '',
+    playlist_name: '',
   });
+  const actionButtons = useDisclosure();
 
+  const router = useRouter();
   const [search, setSearch] = useState('');
   const [selectedVideo, setSeletedVideo] = useState('');
   const [isOpenAddNewVideoModal, setIsOpenAddNewVideoModal] = useState(false);
+  const [isOpenAddNewPlaylistModal, setIsOpenAddNewPlaylistModal] =
+    useState(false);
   const [videoPreviewURL, setVideoPreviewURL] = useState('');
+  const [playlists, setPlaylists] = useState(props.playlists);
 
   const handleOpenVideo = (url: string) => {
     const videoEmbedLink = getVideoEmbedLink(url);
@@ -102,27 +144,65 @@ export default function Home() {
 
   const handleCloseVideo = () => setSeletedVideo('');
 
-  const handleOpenAddNewVideoModal = () => setIsOpenAddNewVideoModal(true);
+  const handleOpenAddNewVideoModal = () => {
+    actionButtons.close();
+    setIsOpenAddNewVideoModal(true);
+  };
+
+  const handleOpenAddNewPlaylistModal = () => {
+    actionButtons.close();
+    setIsOpenAddNewPlaylistModal(true);
+  };
 
   const handleCloseAddNewVideoModal = () => setIsOpenAddNewVideoModal(false);
 
-  const handleSubmitForm = (event: FormEvent) => {
+  const handleCloseAddNewPlaylistModal = () =>
+    setIsOpenAddNewPlaylistModal(false);
+
+  const handleCreateOneVideo = async (event: FormEvent) => {
     event.preventDefault();
 
-    console.log({ values });
+    const { video_title, video_url, video_playlist } = values;
+    const thumbnail = getVideoThumbnailLink(video_url);
 
-    clearFields();
+    if (thumbnail) {
+      await CreateOneVideoService.execute({
+        title: video_title,
+        url: video_url,
+        thumbnail,
+        playlist_id: Number(video_playlist),
+      });
+      router.reload();
+      clearFields();
+      handleCloseAddNewVideoModal();
+    }
+  };
+
+  const handleCreateOnePlaylist = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const { playlist_name } = values;
+
+    if (playlist_name) {
+      await CreateOnePlaylistService.execute({
+        name: playlist_name,
+      });
+      // setPlaylists([...playlists, ...data!]);
+      router.reload();
+      clearFields();
+      handleCloseAddNewPlaylistModal();
+    }
   };
 
   useEffect(() => {
-    const videoEmbedLink = getVideoEmbedLink(values.url);
+    const videoEmbedLink = getVideoEmbedLink(values.video_url);
 
     if (videoEmbedLink) {
       setVideoPreviewURL(videoEmbedLink);
     } else {
       setVideoPreviewURL('');
     }
-  }, [values.url]);
+  }, [values.video_url]);
 
   return (
     <>
@@ -166,7 +246,7 @@ export default function Home() {
             <Playlist.Root key={name}>
               <Playlist.Title>{name}</Playlist.Title>
               <Playlist.Videos>
-                {playlists[name]
+                {props.data[name]
                   .filter((video) =>
                     video.title.toLowerCase().includes(search.toLowerCase()),
                   )
@@ -175,7 +255,7 @@ export default function Home() {
                       key={video.url}
                       onClick={() => handleOpenVideo(video.url)}>
                       <Video.Thumbnail
-                        src={video.thumb}
+                        src={video.thumbnail}
                         width={210}
                         height={118}
                         alt={video.title}
@@ -212,7 +292,7 @@ export default function Home() {
         <Dialog.Portal>
           <Dialog.Overlay />
           <Dialog.Content
-            css={{ maxW: '420px', h: '305px', px: '$4', pb: '$4' }}>
+            css={{ maxW: '420px', h: '370px', px: '$4', pb: '$4' }}>
             <TabsRoot defaultValue="add-new-video-tab">
               <TabsList>
                 <TabsTrigger value="add-new-video-tab">
@@ -221,38 +301,55 @@ export default function Home() {
                 <TabsTrigger value="preview-tab">Pré-visualização</TabsTrigger>
               </TabsList>
               <Tabs.Content value="add-new-video-tab">
-                <Form.Root onSubmit={handleSubmitForm}>
+                <Form.Root onSubmit={handleCreateOneVideo}>
                   <Form.Input>
-                    <Form.Label htmlFor="video-title">
+                    <Form.Label htmlFor="video_title">
                       Título do vídeo
                     </Form.Label>
                     <Form.Field
                       type="text"
                       placeholder="Título do vídeo aqui"
-                      id="video-title"
-                      name="title"
-                      value={values.title}
+                      id="video_title"
+                      name="video_title"
+                      value={values.video_title}
                       onChange={handleChange}
                       autoComplete="none"
                     />
                   </Form.Input>
                   <Form.Input>
-                    <Form.Label htmlFor="video-url">URL do vídeo</Form.Label>
+                    <Form.Label htmlFor="video_url">URL do vídeo</Form.Label>
                     <Form.Field
                       type="text"
                       placeholder="URL do vídeo aqui"
-                      id="video-url"
-                      name="url"
-                      value={values.url}
+                      id="video_url"
+                      name="video_url"
+                      value={values.video_url}
                       onChange={handleChange}
                       autoComplete="none"
                     />
                   </Form.Input>
+                  <Form.Label htmlFor="video_playlist">
+                    Playlist do vídeo
+                  </Form.Label>
+                  <Select
+                    id="video_playlist"
+                    name="video_playlist"
+                    value={values.video_playlist}
+                    onChange={handleChange}>
+                    <Option value="" disabled>
+                      Selecione a playlist
+                    </Option>
+                    {playlists.map((playlist) => (
+                      <Option key={playlist.id} value={playlist.id}>
+                        {playlist.name}
+                      </Option>
+                    ))}
+                  </Select>
                   <Form.SubmitButton>Adicionar</Form.SubmitButton>
                 </Form.Root>
               </Tabs.Content>
               <Tabs.Content value="preview-tab">
-                <Iframe src={videoPreviewURL} css={{ h: '210px' }} />
+                <Iframe src={videoPreviewURL} css={{ h: '285px' }} />
               </Tabs.Content>
             </TabsRoot>
             <Dialog.Close asChild>
@@ -264,7 +361,76 @@ export default function Home() {
         </Dialog.Portal>
       </Dialog.Root>
 
-      <AddNewVideoButton onClick={handleOpenAddNewVideoModal} />
+      <Dialog.Root open={isOpenAddNewPlaylistModal}>
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          <Dialog.Content css={{ maxW: '420px', px: '$4', pb: '$4', pt: '$2' }}>
+            <Dialog.Title>Adicinar playlist</Dialog.Title>
+            <Form.Root onSubmit={handleCreateOnePlaylist} css={{ pt: '$4' }}>
+              <Form.Input>
+                <Form.Label htmlFor="playlist_name">
+                  Nome da playlist
+                </Form.Label>
+                <Form.Field
+                  type="text"
+                  placeholder="Nome da playlist aqui"
+                  id="playlist_name"
+                  name="playlist_name"
+                  value={values.playlist_name}
+                  onChange={handleChange}
+                  autoComplete="none"
+                />
+              </Form.Input>
+              <Form.SubmitButton>Adicionar</Form.SubmitButton>
+            </Form.Root>
+            <Dialog.Close asChild>
+              <Dialog.CloseButton onClick={handleCloseAddNewPlaylistModal}>
+                <X size={24} weight="bold" />
+              </Dialog.CloseButton>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+
+      {actionButtons.isOpen && (
+        <ActionButton.Root>
+          <ActionButton.Item onClick={handleOpenAddNewVideoModal}>
+            Adicionar novo vídeo
+          </ActionButton.Item>
+          <ActionButton.Item onClick={handleOpenAddNewPlaylistModal}>
+            Adicionar nova playlist
+          </ActionButton.Item>
+        </ActionButton.Root>
+      )}
+
+      <AddNewVideoButton onClick={actionButtons.toggle} />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const playlists = await FindManyPlaylistsService.execute();
+  const videos = await FindManyVideosService.execute();
+
+  let data = {} as Record<string, VideoEntity[]>;
+
+  playlists?.forEach((playlist) => {
+    const videosByPlaylist = videos?.filter((video, index) => {
+      const condition = video.playlist_id === playlist.id;
+
+      condition && videos.splice(index, 1);
+      return condition;
+    });
+    data = {
+      ...data,
+      [playlist.name]: videosByPlaylist,
+    };
+  });
+
+  return {
+    props: {
+      playlists,
+      data,
+    },
+  };
+};
