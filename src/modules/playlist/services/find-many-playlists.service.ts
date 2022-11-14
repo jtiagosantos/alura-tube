51b22bab701e8bfdd69899b7755b4d982dad82a8
jtiagosantos <tiagosan040@gmail.com>
2022-11-14 16:23:44 -0300

    feat: include userId

--- src/modules/playlist/services/find-many-playlists.service.ts
+++ src/modules/playlist/services/find-many-playlists.service.ts
@@ -1,15 +1,17 @@
 import { supabase } from '@/common/lib/supabase/client';
 import { PlaylistEntity } from '../entities/playlist.entity';
 import { ETables } from '@/common/enums/tables.enum';
+import type { IFindManyPlaylists } from '../interfaces/find-many-playlists.interface';
 
 export class FindManyPlaylistsService {
-  public static async execute() {
+  public static async execute({ userId }: IFindManyPlaylists) {
     const { PLAYLISTS } = ETables;
 
     const { data } = await supabase
       .from(PLAYLISTS)
       .select<'*', PlaylistEntity>()
-      .order('id', { ascending: true });
+      .order('id', { ascending: true })
+      .eq('user_id', userId);
 
     return data;
   }
