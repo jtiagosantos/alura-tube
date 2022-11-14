d3ae921fdd95ce2aa09617a5dfa1c522e15a3266
jtiagosantos <tiagosan040@gmail.com>
2022-11-14 15:33:30 -0300

    chore: restructure supabase client

--- src/common/lib/supabase/client.ts
+++ src/common/lib/supabase/client.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const NEXT_PUBLIC_SUPABASE_PROJECT_URL =
-  process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL;
+const SUPABASE_PROJECT_URL = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL;
+const SUPABASE_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
 
-const NEXT_PUBLIC_SUPABASE_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
-
-export const supabase = createClient(
-  NEXT_PUBLIC_SUPABASE_PROJECT_URL,
-  NEXT_PUBLIC_SUPABASE_API_KEY,
-);
+export const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_API_KEY);
