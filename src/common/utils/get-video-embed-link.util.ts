350765d1c4e074dac97f16bb02546ea40361b7b5
jtiagosantos <tiagosan040@gmail.com>
2022-11-13 21:15:35 -0300

    chore: change getVideoEmbedLink util function

--- src/common/utils/get-video-embed-link.util.ts
+++ src/common/utils/get-video-embed-link.util.ts
@@ -1,6 +1,6 @@
 export const getVideoEmbedLink = (url: string) => {
-  if (url.includes('=')) {
-    const videoId = url.split('=')[1];
+  if (url.includes('v=')) {
+    const videoId = url.split('v=')[1];
     const videoEmbedLink = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
 
     return videoEmbedLink;
