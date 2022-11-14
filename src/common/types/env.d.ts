dccaa0afec51aeddbfa8eadbbadc56652b015c24
jtiagosantos <tiagosan040@gmail.com>
2022-11-14 15:35:36 -0300

    chore: add some env variables to global type

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -3,6 +3,9 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_SUPABASE_PROJECT_URL: string;
       NEXT_PUBLIC_SUPABASE_API_KEY: string;
+      GITHUB_CLIENT_ID: string;
+      GITHUB_CLIENT_SECRET_KEY: string;
+      NEXTAUTH_SECRET_KEY: string;
     }
   }
 }
